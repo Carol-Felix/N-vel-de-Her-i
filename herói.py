d9972f61167688ec1e6305f4dcc2cace0c5fbc80
Carol-Felix <167262570+Carol-Felix@users.noreply.github.com>
2024-05-19 15:57:25 +0000

    Pending changes exported from your codespace

---
+++ herói.py
@@ -0,0 +1,30 @@
+nome = input() 
+
+experiencia = input() 
+
+if experiencia < 1000: 
+    print("Ferro") 
+
+elif experiencia ==> 1001 and 2000: 
+    print("Bronze") 
+
+ elif experiencia ==> 2001 and 5000: 
+    print("Prata")
+
+elif experiencia ==> 5001 and 7000: 
+    print("Ouro")
+
+elif experiencia ==> 7001 and 8000: 
+    print("Platina")  
+
+elif experiencia ==> 8001 and 9000: 
+    print("Ascendente")
+
+elif experiencia ==> 9001 and 10000: 
+    print("Imortal") 
+
+else experiencia ==> 10001: 
+    print("Radiante")
+
+
+print("O Herói de nome {nome} está no nível de {experiencia}")
